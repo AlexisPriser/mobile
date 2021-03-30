69c15012a8c1144ef09a98e68f95ab0c23310494
AlexisPriser <priseralexis78@gmail.com>
2021-03-30 08:58:51 +0200

    cour aprem

--- src/app/pages/login/login.page.ts
+++ src/app/pages/login/login.page.ts
@@ -1,4 +1,7 @@
 import { Component, OnInit } from '@angular/core';
+import { AuthService } from '../../services/auth.service';
+import { LoadingController } from '@ionic/angular';
+import { Router } from '@angular/router';
 
 @Component({
   selector: 'app-login',
@@ -6,10 +9,34 @@ import { Component, OnInit } from '@angular/core';
   styleUrls: ['./login.page.scss'],
 })
 export class LoginPage implements OnInit {
+  email: string = 'eee';
+  password: string = 'eee';
+  constructor(
+    private router: Router,
+    private auth: AuthService,
+    private loading: LoadingController
+  ) {}
 
-  constructor() { }
+  ngOnInit() {}
 
-  ngOnInit() {
+  async loginForm() {
+    const load = await this.loading.create({
+      cssClass: 'myCustomClass',
+      message: 'please wait...',
+    });
+
+    await load.present();
+    console.log('email', this.email);
+    console.log('password', this.password);
+
+    this.auth.getProfile().subscribe((data) => {
+      console.log('data', data);
+    });
+
+    this.auth.getProfile().subscribe(async (user) => {
+      console.log('user', user);
+      this.router.navigate(['/home']);
+      await load.onDidDismiss();
+    });
   }
-
 }
---
+++ src/app/services/auth.service.ts
@@ -0,0 +1,13 @@
+import { HttpClient } from '@angular/common/http';
+import { Injectable } from '@angular/core';
+
+@Injectable({
+  providedIn: 'root',
+})
+export class AuthService {
+  url: string = 'http://localhost:3000';
+  constructor(private http: HttpClient) {}
+  getProfile() {
+    return this.http.get(this.url + 'profil');
+  }
+}
